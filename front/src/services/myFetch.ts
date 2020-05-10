28ffdf135a8a04348b562a524640236254c25e0f
Erwan de Lépinau <erwan@feedly.com>
2020-05-10 15:28:57 -0700

    add back-end communication services

---
+++ front/src/services/myFetch.ts
@@ -0,0 +1,39 @@
+type RequestMethod = "GET" | "POST" | "PUT" | "DELETE";
+
+const baseUrl = new URL("http://localhost:5000");
+
+async function myFetch(
+    endpoint: string,
+    method: RequestMethod = "GET",
+    body?: string,
+): Promise<any> {
+    const response = await fetch(baseUrl + endpoint, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method,
+        body,
+    });
+    let responseData: string;
+    try {
+        responseData = await response.json();
+    } catch {
+        responseData = "No data provided";
+    }
+    return response.ok ? responseData : Promise.reject(responseData);
+}
+
+export async function apiGet(endpoint: string): Promise<any> {
+    return myFetch(endpoint, "GET");
+}
+
+export async function apiPost(endpoint: string, body: string): Promise<any> {
+    return myFetch(endpoint, "POST", body);
+}
+export async function apiPut(endpoint: string, body: string): Promise<any> {
+    return myFetch(endpoint, "PUT", body);
+}
+
+export async function apiDelete(endpoint: string): Promise<any> {
+    return myFetch(endpoint, "DELETE");
+}
---
+++ front/src/services/tasksService.ts
@@ -0,0 +1,14 @@
+import { apiGet, apiPut } from "@/services/myFetch.ts";
+import { TaskType } from "@/types";
+
+export async function getUserData(username: string): Promise<TaskType[]> {
+    const responseData = await apiGet(`users/${username}`);
+    return responseData;
+}
+
+export async function putUpdatedUserData(
+    username: string,
+    data: TaskType[],
+): Promise<void> {
+    await apiPut(`users/${username}`, JSON.stringify(data));
+}
